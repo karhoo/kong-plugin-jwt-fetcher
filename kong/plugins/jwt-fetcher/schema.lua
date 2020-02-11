247bfeb3b1ce98688ae939d7515cdd2b7d4507a3
Tomasz Nowak <tomasz.nowak@karhoo.com>
2020-02-11 22:49:49 +0100

    [INF-787] Make plugin work with kong v1

--- kong/plugins/jwt-fetcher/schema.lua
+++ kong/plugins/jwt-fetcher/schema.lua
@@ -1,61 +1,44 @@
+local typedefs = require "kong.db.schema.typedefs"
+
+
 return {
-  no_consumer = true,
+  name = "jwt-fetcher",
   fields = {
-    url = {
-      -- URL of remote server where to fetch the JWT
-      -- Can use HTTP/HTTPS
-      type = "string",
-      required = true,
-    },
-    query_key = {
-      -- the query key/name in which to store the custom id send to the
-      -- remote server
-      type = "string",
-      default = "username",
-      required = true,
-    },
-    response_key = {
-      -- the key, in the JSON body response that contains the JWT. Assumes the
-      -- body to be a JSON object. If not provided, it will assume the JWT
-      -- in in a top-level JSON string value
-      type = "string",
-      default = "access_token",
-    },
-    timeout = {
-      -- timeout when making the http request to fetch the JWT (in ms)
-      type = "integer",
-      default = 60000,
-      required = true,
-    },
-    keepalive = {
-      -- connection keepalive (in ms)
-      type = "integer",
-      default = 60000,
-      required = true,
-    },
-    shm = {
-      -- the shm to use as node level cache
-      type = "string",
-      default = "jwtstore",
-      required = true,
-    },
-    negative_ttl = {
-      -- if a JWT is not on the remote server, how long to cache that fact
-      -- (in ms)
-      type = "integer",
-      default = 10000,
-      required = true,
-    },
-    skew = {
-      -- Max clock skew to compensate ('exp' claim is extended with
-      -- this) in seconds
-      type = "integer",
-      default = 0,
-      required = true,
-    },
+    { consumer = typedefs.no_consumer },
+    { protocols = typedefs.protocols_http },
+    { config = {
+        type = "record",
+        fields = {
+          -- URL of remote server where to fetch the JWT
+          -- Can use HTTP/HTTPS
+          { url = { type = "string", required = true }, },
+
+          -- the query key/name in which to store the custom id send to the
+          -- remote server
+          { query_key = { type = "string", default = "username", required = true }, },
+
+          -- the key, in the JSON body response that contains the JWT. Assumes the
+          -- body to be a JSON object. If not provided, it will assume the JWT
+          -- in a top-level JSON string value
+          { response_key = { type = "string", default = "access_token" }, },
+
+          -- timeout when making the http request to fetch the JWT (in ms)
+          { timeout = { type = "integer", default = 60000, required = true }, },
+
+          -- connection keepalive (in ms)
+          { keepalive = { type = "integer", default = 60000, required = true }, },
+
+          -- the shm to use as node level cache
+          { shm = { type = "string", default = "jwtstore", required = true }, },
+
+          -- if a JWT is not on the remote server, how long to cache that fact
+          -- (in ms)
+          { negative_ttl = { type = "integer", default = 10000, required = true }, },
+
+          -- Max clock skew to compensate ('exp' claim is extended with
+          -- this) in seconds
+          { skew = { type = "integer", default = 0, required = true }, },
+        },
+    }, },
   },
-  self_check = function(schema, plugin_t, dao, is_updating)
-    -- TODO: perform any custom verification?
-    return true
-  end
 }
